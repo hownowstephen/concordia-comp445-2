c4040deebd7a961907b5e5528c296332dbb66b46
Stephen Young <stephen@tryllo.com>
2012-03-09 00:20:34 -0500

    packet num is already a pointer in protocol recursion

--- protocol.cpp
+++ protocol.cpp
@@ -56,7 +56,7 @@ int sendbuf(SOCKET sock, SOCKADDR_IN sa, int* packet_num, char* buffer,int buffe
             }
         }else{
             // Otherwise re-initiate the process
-            return sendbuf(sock, sa, &packet_num, buffer, buffer_size);
+            return sendbuf(sock, sa, packet_num, buffer, buffer_size);
         }
     }
 }
@@ -93,7 +93,7 @@ int recvbuf(SOCKET sock, SOCKADDR_IN sa, int* packet_num, char* buffer, int buff
             }
         }
     }else{
-        return recvbuf(sock, sa, &packet_num, buffer, buffer_size);
+        return recvbuf(sock, sa, packet_num, buffer, buffer_size);
     }
     
 }
