309ed68aceba1d73aa84f8bffe2f909c7a2c3840
Stephen Young <stephen@tryllo.com>
2012-03-11 18:20:02 -0400

    cant use socket as name + fixed missing semicolon

--- protocol.cpp
+++ protocol.cpp
@@ -26,7 +26,7 @@ using namespace std;
 
 
 SOCKET open_port(int port){
-    SOCKET socket;      // Define the socket to return
+    SOCKET sock;      // Define the socket to return
     SOCKADDR_IN sa;     // Define the socket address information
     HOSTENT *hp;        // Host entity details
     char hostname[11]; // Store the value of localhost
@@ -40,17 +40,17 @@ SOCKET open_port(int port){
     sa.sin_port = htons(port);
     sa.sin_addr.s_addr = htonl(INADDR_ANY);
      // Create the socket
-    if((socket = socket(AF_INET,SOCK_DGRAM,0))==INVALID_SOCKET) throw "Generating a new local socket failed";
+    if((sock = socket(AF_INET,SOCK_DGRAM,0))==INVALID_SOCKET) throw "Generating a new local socket failed";
     // Bind to the client port
-    if (bind(socket,(LPSOCKADDR)&sa,sizeof(sa)) == SOCKET_ERROR) throw "Could not bind socket to supplied port";
+    if (bind(sock,(LPSOCKADDR)&sa,sizeof(sa)) == SOCKET_ERROR) throw "Could not bind socket to supplied port";
 
-    return socket;
+    return sock;
 }
 
 SOCKADDR_IN prepare_peer_connection(char* hostname, int port){
     SOCKADDR_IN sa;
     HOSTENT *hp;
-    if((hp==gethostbyname(hostname)) == NULL) throw "Could not determine a host address from supplied name"
+    if((hp==gethostbyname(hostname)) == NULL) throw "Could not determine a host address from supplied name";
 
     // Fill in port and address information
     memcpy(&sa.sin_addr,hp->h_addr,hp->h_length);
